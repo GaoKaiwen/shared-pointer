ec69ae13c7101db412e541997280ff72e7db0a16
gaoka <gaoxkaiwen@gmail.com>
2024-05-17 22:34:49 -0300

    Implement server and client functionality

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
-const path = require('path')
-const app = express()
+const { createServer } = require("http")
+const { Server } = require("socket.io")
 
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+io.on('connection', socket => {
+    console.log('User connected')
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile('C:/Users/gaoka/workspace-estudos/shared-pointer/index.html')
 })
 
+app.post('/', (req, res) => {
+    console.log(`Mouse X position: ${req.body.mousePositionX}`)
+    console.log(`Mouse Y position: ${req.body.mousePositionY}`)
+    res.status(200).send('Mouse positions received successfully')
+
+    io.emit('position', JSON.stringify({
+        id: req.hostname,
+        mousePositionX: req.body.mousePositionX,
+        mousePositionY: req.body.mousePositionY
+    }))
+})
 
 let port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Opening server on ${port} port`))
+httpServer.listen(port, () => console.log(`Opening server on ${port} port`))
